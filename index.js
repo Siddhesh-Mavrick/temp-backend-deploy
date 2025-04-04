93faf61edeec7d7ecb0ff9ac637d91cd1d95ffd8
Siddhesh-Mavrick <siddheshbhise04@gmail.com>
2025-04-04 20:09:41 +0530

    origin changed

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser())
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: true,
     credentials: true,
 }));
 
